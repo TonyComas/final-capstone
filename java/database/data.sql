69e413425fb5d96d056512c8179146a7b5706997
Michael Sise <michaelsise727272@gmail.com>
2023-08-07 09:19:28 -0400

    initial database draft

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,46 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO video_games (game_name, description, developer, publisher, release_date) VALUES
+('')
+('')
+('')
+('')
+('')
+('')
+('')
+('')
+('')
+('')
+('')
+('')
+
+INSERT INTO game_genre (game_id, genre_id) VALUES
+(1, 1)
+(1, 1)
+(1, 1)
+(1, 1)
+(1, 1)
+(1, 1)
+(1, 1)
+(1, 1)
+(1, 1)
+(1, 1)
+(1, 1)
+
+INSERT INTO genre (genre_name) VALUES
+('Action'),
+('Adventure'),
+('Role-playing game'),
+('Fantasy'),
+('Simulation'),
+('Strategy'),
+('First-person shooter'),
+('Sports'),
+('Puzzle'),
+('Horror'),
+('Fighting'),
+('Survival'),
+('Massively multiplayer');
+
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,5 @@
-BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, video_games, game_genre, genre CASCADE;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -10,4 +9,28 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
-COMMIT TRANSACTION;
+CREATE TABLE video_games (
+	game_id SERIAL,
+	game_name varchar(100) NOT NULL UNIQUE,
+	description varchar(500) NOT NULL,
+	release_date varchar(50) NOT NULL,
+	developer varchar(100) NOT NULL,
+	publisher varchar(100),
+	CONSTRAINT PK_game PRIMARY KEY (game_id)
+);
+
+CREATE TABLE game_genre (
+	game_id int NOT NULL,
+	genre_id int NOT NULL,
+	game_genre_id SERIAL,
+	CONSTRAINT PK_genre PRIMARY KEY (game_genre_id),
+	CONSTRAINT FK_game FOREIGN KEY (game_id) REFERENCES video_games (game_id),
+	CONSTRAINT FK_genre FOREIGN KEY (genre_id) REFERENCES genre (genre_id)
+);
+
+CREATE TABLE genre (
+	genre_id SERIAL,
+	genre_name varchar(100),
+	CONSTRAINT PK_genre PRIMARY KEY (genre_id)
+);
+
