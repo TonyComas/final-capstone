4160066c56bd9067762202fd79e72ee518eefbf5
Thad Osburn <thadmsosburn@outlook.com>
2023-08-07 12:22:47 -0400

    Lunch Time

--- java/src/main/java/com/techelevator/controller/GameController.java
+++ java/src/main/java/com/techelevator/controller/GameController.java
@@ -2,12 +2,14 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.GameDao;
 import com.techelevator.model.Game;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping (path = "games")
 public class GameController {
---
+++ vue/src/views/Games.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="games">
+      <GameListVue />
+  </div>
+</template>
+
+<script>
+import GameListVue from '../components/GameList.vue'
+export default {
+    components: {
+        GameListVue
+    }
+
+}
+</script>
+
+<style>
+
+</style>
