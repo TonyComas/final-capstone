296e06bf0ed3947532d7cd9e0c1d9d46aa8ad979
Michael Sise <michaelsise727272@gmail.com>
2023-08-09 10:08:28 -0400

    added title to header for games list page

--- vue/src/components/PageHeader.vue
+++ vue/src/components/PageHeader.vue
@@ -3,10 +3,12 @@
       <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
        <router-link :to="{ name: 'AddGame' }" > Add a Game!</router-link>&nbsp;|&nbsp;
-       <router-link :to="{ name: 'games' }">Game List | </router-link>
+       <router-link :to="{ name: 'games' }">Game List</router-link>
        <!-- v-if="$store.state.token"  this goes ^ -->
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
+    <h1 class = "page-title">Welcome to our Video Game Database!</h1>
+    <div class = "spacer"></div>
   </header>
 </template>
 
@@ -21,10 +23,26 @@ export default {
 <style>
 header {
   display: flex;
-  align-items: center;
   justify-content: space-between;
+  align-items: center;
   height: 100px;
+  
   border-bottom: 2px darkblue solid;
+}
+
+#nav {
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+}
+
+.page-title {
+  font-size: 36px;
+  margin: 0;
+}
+
+.spacer {
+  flex-grow: 1;
 }
 
 </style>
