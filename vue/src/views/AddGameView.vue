aa51769692885a24c682596a3ef64a28e16fccf4
Michael Sise <michaelsise727272@gmail.com>
2023-08-10 16:56:36 -0400

    add game styling

--- vue/src/views/AddGameView.vue
+++ vue/src/views/AddGameView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <h1>Add A New Game!</h1>
+      <h1 class ="add-game">Add A New Game!</h1>
         <AddGameVue :fullGame="{}"/>
   </div>
 </template>
@@ -15,5 +15,36 @@ export default {
 </script>
 
 <style>
+h1 {
+  font-size: 24px;
+}
+
+.add-game {
+   text-transform: uppercase;
+  background-image: linear-gradient(
+    -225deg,
+    #281372 0%,
+    #6c23b9 29%,
+    #ff1361 67%,
+    #1eff00 100%
+  );
+  background-size: auto auto;
+  background-clip: border-box;
+  background-size: 200% auto;
+  color: #fff;
+  background-clip: text;
+  text-fill-color: transparent;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: textclip 2s linear infinite;
+  display: inline-block;
+      font-size: 20px;
+}
+
+@keyframes textclip {
+  to {
+    background-position: 200% center;
+  }
+}
 
 </style>
