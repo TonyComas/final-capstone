fc5b4ffc895320c85e74b124f5e9045d60c44435
Thad Osburn <thadmsosburn@outlook.com>
2023-08-11 14:58:42 -0400

    Update

--- vue/src/components/AddReview.vue
+++ vue/src/components/AddReview.vue
@@ -2,7 +2,7 @@
   <form v-on:submit.prevent="addNewReview">
   
     <div class="form-element">
-      <label for="title" class = "text-headers">Review Title:</label>
+      <label for="title">Review Title:</label>
       <input id="title" type="text" v-model="newReview.review_title" />
     </div>
     <div class="form-element">
@@ -16,12 +16,12 @@
       </select>
     </div>
     <div class="form-element">
-      <label for="review">Review: </label>
+      <label for="review">Review</label>
       <textarea id="review" v-model="newReview.review_body"></textarea>
     </div>
     <div class="actions">
-      <button v-on:click="returnToGamePage()" type="button" class = "review-button cancel-button">Cancel</button>
-      <button  class = "review-button submit-button">Submit</button>
+      <button v-on:click="returnToGamePage()" type="button">Cancel</button>
+      <button>Submit</button>
     </div>
   </form>
 </template>
@@ -66,6 +66,7 @@ export default {
       this.newReview = {};
     },
     returnToGamePage() {
+
       this.$router.push( {
         name: 'single-game-view',
         params: {
@@ -73,7 +74,6 @@ export default {
         }
      
       })
-          this.resetForm()  
     }
   },
   created() {
@@ -90,69 +90,19 @@ form {
 .form-element label {
   width: 100px;
   vertical-align: top;
-  color: rgb(11, 226, 11);
 }
 .form-element input,
 select,
 textarea {
   width: 400px;
-  font-size: 11pt;
-  background: black;
-  color: rgb(11, 226, 11);
-  font-family: 'Press Start 2P', cursive;
-  font-weight: 50;
-  margin: 2px;
-  padding: 5px;
-
-  
+  font-size: 1.1rem;
 }
-
-.review-button {
-  font-size: 10px;
-  /* height: 30px; */
-  /* border-radius: 30px; */
-  /* border-width: 1px; */
-  text-align: right;
-  box-sizing: border-box;
-  /* -webkit-appearance: none;
-  -moz-appearance: none; */
-  appearance: none;
-  background-color: transparent;
-  /* border: 2px solid #e74c3c; */
-  border: 2px solid rgb(11, 226, 11);
-  border-radius: 0.2em;
-  /* color: #e74c3c; */
-  color: rgb(11, 226, 11);
-  font-family: 'Press Start 2P', cursive;
-  font-weight: 50;
-  /* cursor: pointer; */
-  /* display: -webkit-box;
-  display: -webkit-flex;
-  display: -ms-flexbox; */
-  display: flex;
-  /* -webkit-align-self: center;
-  -ms-flex-item-align: center; */
-  /* align-self: center; */
-  /* font-size: 0.8rem; */
-  line-height: 1;
-  margin: 5px;
-  padding: 0.5em 1em;
-  /* text-decoration: none; */
-  text-align: center;
-  text-transform: uppercase;
-  /* position: relative; */
-  /* z-index: 1; */
-  transition: box-shadow 300ms ease-in-out, color 300ms ease-in-out;
-  display:inline-block;
-}
-
-
 .form-element textarea {
   height: 150px;
 }
-/* .actions {
+.actions {
   float: right;
-} */
+}
 .actions button {
   margin-left: 10px;
 }
