03f0c8388db12a2bd7c64c448af512d2c1850855
Michael Sise <michaelsise727272@gmail.com>
2023-08-15 10:12:09 -0400

    fancy border on asteroids - worked on pong one

--- vue/src/components/Asteroids.vue
+++ vue/src/components/Asteroids.vue
@@ -1,6 +1,18 @@
 <template>
-    <div>
+    <div class="center-container">
+    <div class="game-container">
+        <div class="outer-circle">
+    <div class="inner-circle">
+          <div class="asteroids">
         <iframe id="gameFrame" :src="htmlPath" frameborder="0"></iframe>
+        </div>
+    </div>
+    </div>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
     </div>
 </template>
 
@@ -18,10 +30,69 @@ export default {
 
 <style>
 iframe#gameFrame{
-    margin-top: 50px;
+    margin-top: 30px;
     width: 790px;
     height: 600px;
-    border-width: 5px;
-    border-color: green;
+    z-index: 1;
+}
+
+.asteroids {
+    text-align: center;
+}
+
+.center-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+
+.outer-circle {
+  position: relative;
+  height: 640px;
+  width: 800px;
+  background: linear-gradient(#14ffe9, #ffeb3b, #ff00e0);
+  animation: rotate 6.5s linear infinite;
+  z-index: 0;
+  top: 10px;
+}
+
+.outer-circle span {
+  position: absolute;
+  height: 640px;
+  width: 800px;
+  background: linear-gradient(#14ffe9, #ffeb3b, #ff00e0);
+  z-index: 0;
+}
+
+span:nth-child(2) {
+  filter: blur(10px);
+}
+
+span:nth-child(3) {
+  filter: blur(25px);
+}
+
+span:nth-child(4) {
+  filter: blur(150px);
+}
+
+.inner-circle {
+  height: 620px;
+  width: 780px;
+  position: absolute;
+  background: black;
+  top: 10px;
+  left: 10px;
+  z-index: 2;
+}
+
+@keyframes rotate {
+  0% {
+    filter: hue-rotate(0deg);
+  }
+  100% {
+    filter: hue-rotate(360deg);
+  }
 }
 </style>
