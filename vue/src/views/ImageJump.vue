f9ba0eb4cdd2df8701be752a1fbabd2e1711b237
Michael Sise <michaelsise727272@gmail.com>
2023-08-16 10:26:05 -0400

    checkboxes in single game + extra buttons

---
+++ vue/src/views/ImageJump.vue
@@ -0,0 +1,33 @@
+<template>
+  <div @keydown.space="jump" tabindex="0">
+    <img
+      :src="imageUrl"
+      alt="Jumping Image"
+      :style="{ transform: `translateY(${jumpHeight}px)` }"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+    return {
+      imageUrl: '../assets/luigi-talk.gif',
+      jumpHeight: 50,
+    };
+  },
+  methods: {
+    jump() {
+      
+      this.jumpHeight = -40; 
+      setTimeout(() => {
+        this.jumpHeight = 0; 
+      }, 300);
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
